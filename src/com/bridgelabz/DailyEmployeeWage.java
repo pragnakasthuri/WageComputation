ccc6a576f665a14fa371b20b6579f98000f803dc
pragnakasthruri <kpragna25@gmail.com>
2022-02-15 07:18:20 +0530

    Added code for Daily Employee Wage Computation

---
+++ src/com/bridgelabz/DailyEmployeeWage.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class DailyEmployeeWage {
+    public static void main(String[] args) {
+        int WagePerHour = 20, FullDayHour = 8;
+        int DailyEmployeeWage = WagePerHour * FullDayHour;
+        System.out.println(DailyEmployeeWage);
+    }
+}
+
