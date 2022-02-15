cd8ba8abdff8381f6086184702121908eef5043b
pragnakasthruri <kpragna25@gmail.com>
2022-02-15 17:06:11 +0530

    adding the changes

--- src/com/bridgelabz/EmployeeAttendanceUC1.java
+++ src/com/bridgelabz/EmployeeAttendanceUC1.java
@@ -3,8 +3,8 @@ package com.bridgelabz;
 public class EmployeeAttendanceUC1 {
     public static void main(String[] args) {
         int Is_Full_Time = 1;
-        double EmpAttendanceCheck = Math.floor(Math.random() * 10) % 2;
-        if (EmpAttendanceCheck == Is_Full_Time)
+        double empAttendanceCheck = Math.floor(Math.random() * 10) % 2;
+        if (empAttendanceCheck == Is_Full_Time)
             System.out.println("Employee is present");
         else
             System.out.println("Employee is absent");
