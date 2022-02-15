86f5a79d3c14bbf6bd30dc29b3bb6d2503548c29
pragnakasthruri <kpragna25@gmail.com>
2022-02-15 17:28:53 +0530

    adding emp wage program for given condition

---
+++ src/com/bridgelabz/EmployeeWageConditionUC6.java
@@ -0,0 +1,33 @@
+package com.bridgelabz;
+
+public class EmployeeWageConditionUC6 {
+    public static final int Is_Part_Time = 1;
+    public static final int Is_Full_Time = 2;
+    public static final int Wage_Per_Hour = 20;
+    public static final int Num_Of_Working_Days = 20;
+    public static final int Max_Num_Of_Hrs = 100;
+
+    public static void main(String[] args) {
+        int empHrs = 0, empDailyWage = 0, empMonthlyWage = 0, totalWorkingDays = 0, totalEmpHrs = 0;
+        while (totalEmpHrs <= Max_Num_Of_Hrs && totalWorkingDays <= Num_Of_Working_Days ){
+            totalWorkingDays++;
+            int empAttendanceCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empAttendanceCheck) {
+                case Is_Full_Time:
+                    empHrs = 8;
+                    break;
+                case Is_Part_Time:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            totalEmpHrs += empHrs;
+            empDailyWage = empHrs * Wage_Per_Hour;
+            empMonthlyWage = empMonthlyWage + empDailyWage;
+            System.out.println("Employee Daily Wage : " + empDailyWage);
+        }
+        System.out.println("Employee Monthly Wage : " + empMonthlyWage);
+    }
+}
+
